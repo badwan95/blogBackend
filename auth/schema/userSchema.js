36efa4c8e193b478217e46d51b196563c058b0e8
Ammar <ammar.badwan95@gmail.com>
2020-09-17 16:35:21 +0300

    added basic authentication login

--- auth/schema/userSchema.js
+++ auth/schema/userSchema.js
@@ -17,4 +17,32 @@ users.pre('save',async function(){
   this.password = await bcrypt.hash(this.password,5); //To hash the password
 });
 
+users.statics.authenticateBasic = async function(username,password){
+  let theUser = await this.findOne({username});
+  let valid = await bcrypt.compare(password,theUser.password);
+  return valid ? theUser : Promise.reject();
+};
+
+users.statics.generateToken = function(user){
+  let token = jwt.sign({username: user.username ,_id:user._id,userRole:user.role},SECRET,{expiresIn:TOKEN_TIMEOUT});
+  return token;
+};
+
+users.statics.verifyToken = function (token){
+  return jwt.verify(token,SECRET, async function(err,decoded){
+    if(err){
+      console.log('Invalid token, Error: ' + err);
+      return Promise.reject(err);
+    }
+    let username = decoded.username;
+    let theUser = await mongoose.model('users',users).findOne({username});
+  
+    if(theUser){
+      return Promise.resolve(decoded);
+    }
+    return Promise.reject();
+  });
+};
+
+
 module.exports = mongoose.model('users',users);
